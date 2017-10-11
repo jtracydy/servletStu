a88f6c25f7351479045b83a2fb722360410d83e2
dan.wang <dan.wagn@msxf.com>
2017-10-11 19:46:12 +0800

    17-10-11-19-46

--- src/com/t/test/TestServletConfig.java
+++ src/com/t/test/TestServletConfig.java
@@ -1,8 +1,13 @@
 package com.t.test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -24,9 +29,36 @@ public class TestServletConfig extends HttpServlet {
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
+		// super.init(config);
 		String userName = config.getInitParameter("userName");
 		System.out.println(userName);
+		System.out.println("调用 init()方法带参数的方法 ");
+
+		/**
+		 * 获取web.xml中配置的参数
+		 */
+		System.out.println("获取web.xml中配置的参数....");
+		ServletContext context = config.getServletContext();
+		String nameA = context.getInitParameter("A");
+		System.out.println("nameA:" + nameA);
+		String nameB = context.getInitParameter("A");
+		System.out.println("nameB:" + nameB);
+		/**
+		 * 获取properties文件内容
+		 */
+		System.out.println("获取properties文件内容....");
+		try {
+			URL url = context.getResource("/WEB-INF/config/jdbc.properties");// 得到URL
+			InputStream in_url = url.openStream();// 直接得到流
+			// getProperties(in_url);// 获取资源文件
+			/**
+			 * 读取配置文件是通过流
+			 */
+			getProperties(context.getResourceAsStream("/WEB-INF/config/jdbc.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -46,5 +78,17 @@ public class TestServletConfig extends HttpServlet {
 	public void destroy() {
 		System.out.println("调用 destroy()方法 ");
 		super.destroy();
+	}
+
+	private void getProperties(InputStream in) throws IOException {
+		Properties props = new Properties();
+		props.load(in);
+
+		String url = props.getProperty("url");
+		System.out.println("url:" + url);
+		String username = props.getProperty("username");
+		System.out.println("username:" + username);
+		String pwd = props.getProperty("password");
+		System.out.println("pwd:" + pwd);
 	}
 }
